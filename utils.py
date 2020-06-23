6b85fc13f3a2f2bb5b3b6943c9a4aad8d7517d1b
Claudio Manoel da Silva e Sousa Neto <claudio_manoel2005@yahoo.com.br>
2020-06-23 20:20:43 -0300

    Teste adição arquivo

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import os
-#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dialog_key.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dialog_key.json"
 
 import dialogflow_v2 as dialogflow
 dialogflow_session_client = dialogflow.SessionsClient()
